7f23181dc003aa7807efe8537509438fdc3845f9
ranjan-abhijeet <ranjan.viper@outlook.com>
2022-09-21 21:05:40 +0530

    add cross origin resource sharing policy in code

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import user, post, auth, vote
@@ -7,7 +8,24 @@ from .config import settings
 
 app = FastAPI()
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Live long and prosper"}
